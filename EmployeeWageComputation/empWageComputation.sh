b55f60880392b38dd9d5dc5b3466a888e52f0817
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-27 00:24:58 +0530

    added use case 7 use function to get work hours

--- EmployeeWageComputation/empWageComputation.sh
+++ EmployeeWageComputation/empWageComputation.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 echo "Employee Wage Computation"
+
+function get_work_hrs() {
+type=$1
+hour=$2
+if (( $type==0 ))
+then
+	total_working_hrs=$(( $hour+0 ))
+	echo $total_working_hrs
+elif (( $type==1 ))
+then
+   total_working_hrs=$(( $hour+4 ))
+   echo $total_working_hrs
+elif (( $type==2 ))
+then
+   total_working_hrs=$(( $hour+8 ))
+   echo $total_working_hrs
+fi
+}
+
 salary=0
 days=1
 over=0
+Working_hrs=0
 while (( $over==0 ))
 do
 	attend=$(( $RANDOM%3 ))
@@ -25,11 +45,18 @@ do
 		;;
 	esac
 	days=$(( $days+1 ))
-	Working_hrs=$(( $Working_hrs+$DayPrHr ))
+	Working_hrs=$(get_work_hrs $attend $Working_hrs)
 	salary=$(( $salary+($wagePrHr*$DayPrHr) ))
 	if (( $days==20 || $Working_hrs==100 ))
 	then
-		over=1
+		if (( $days==20 ))
+		then
+			echo "20 days over"
+			over=1
+		else
+			echo "100 hours over"
+			over=1
+		fi
 	fi
 done
 echo "Monthly Salary is: " $salary

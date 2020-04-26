915bc72ea1f55345630c11cd5db1b972a439adb9
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-26 23:43:23 +0530

    added use case 1 daily wage of employee

--- EmployeeWageComputation/empWageComputation.sh
+++ EmployeeWageComputation/empWageComputation.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+wagePrHr=20
+FullDayHr=8
 echo "Employee Wage Computation"
 attend=$(( $RANDOM%2 ))
 if (( $attend==0 ))
 then
 	echo "Employee Absent"
+	salary=0
 else
 	echo "Employee Present"
+	salary=$(( $wagePrHr*$FullDayHr ))
 fi
+echo $salary

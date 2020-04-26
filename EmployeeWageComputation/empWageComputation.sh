78e16ff49e01099987aa128288edbe1bfe96f709
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-27 00:04:56 +0530

    added use case 5 calculate monthly wage

--- EmployeeWageComputation/empWageComputation.sh
+++ EmployeeWageComputation/empWageComputation.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 echo "Employee Wage Computation"
-attend=$(( $RANDOM%3 ))
-case $attend in
-0)
-	echo "Employee Absent"
-	wagePrHr=0
-	DayPrHr=0
-	;;
-1)
-	echo "Employee Part time"
-	wagePrHr=20
-	DayPrHr=4
-	;;
-2)
-	echo "Employee Full time"
-	wagePrHr=20
-	DayPrHr=8
-	;;
-esac
-salary=$(( $wagePrHr*$DayPrHr ))
-echo $salary
+salary=0
+for (( i=1;i<=20;i=$i+1 ))
+do
+	attend=$(( $RANDOM%3 ))
+	case $attend in
+	0)
+		echo "Employee Absent"
+		wagePrHr=0
+		DayPrHr=0
+		;;
+	1)
+		echo "Employee Part time"
+		wagePrHr=20
+		DayPrHr=4
+		;;
+	2)
+		echo "Employee Full time"
+		wagePrHr=20
+		DayPrHr=8
+		;;
+	esac
+	salary=$(( $salary+($wagePrHr*$DayPrHr) ))
+done
+echo "Salary is: " $salary

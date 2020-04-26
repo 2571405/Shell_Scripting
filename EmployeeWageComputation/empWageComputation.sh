4bce2dec694f6e6a2561e1102a8fc9aa52a12525
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-26 23:56:48 +0530

    added use case 4 solved using switch case

--- EmployeeWageComputation/empWageComputation.sh
+++ EmployeeWageComputation/empWageComputation.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-wagePrHr=20
-FullDayHr=8
-HalfDayHr=4
 echo "Employee Wage Computation"
 attend=$(( $RANDOM%3 ))
-if (( $attend==0 ))
-then
+case $attend in
+0)
 	echo "Employee Absent"
-	salary=0
-elif (( $attend==1 ))
-then
-	echo "Employee Par time"
-	salary=$(( $wagePrHr*$HalfDayHr ))
-else
+	wagePrHr=0
+	DayPrHr=0
+	;;
+1)
+	echo "Employee Part time"
+	wagePrHr=20
+	DayPrHr=4
+	;;
+2)
 	echo "Employee Full time"
-	salary=$(( $wagePrHr*$FullDayHr ))
-fi
+	wagePrHr=20
+	DayPrHr=8
+	;;
+esac
+salary=$(( $wagePrHr*$DayPrHr ))
 echo $salary

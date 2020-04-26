c000c14e455ae886a5996ffa60182bc30b5c26da
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-26 22:08:21 +0530

    added use case 1 (firstname)

--- Pattern/user_registration.sh
+++ Pattern/user_registration.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "User Registration Program"
-
+read -p "Enter first name: " fname
+fname_pattern="^[[:upper:]][[:lower:]]{2,}$"
+if [[ $fname =~ $fname_pattern ]]
+then
+	echo "Valid first name"
+else
+	echo "Invalid first name!"
+fi

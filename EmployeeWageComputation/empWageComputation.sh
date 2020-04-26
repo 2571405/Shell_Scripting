c7b082ccd57c35729d0d5da14586d6032cc740f9
samiksha.karmakar96@gmail.com <samiksha.karmakar96@gmail.com>
2020-04-26 23:49:22 +0530

    added use case 3 added part time employee and wage

--- EmployeeWageComputation/empWageComputation.sh
+++ EmployeeWageComputation/empWageComputation.sh
@@ -2,14 +2,19 @@
 
 wagePrHr=20
 FullDayHr=8
+HalfDayHr=4
 echo "Employee Wage Computation"
-attend=$(( $RANDOM%2 ))
+attend=$(( $RANDOM%3 ))
 if (( $attend==0 ))
 then
 	echo "Employee Absent"
 	salary=0
+elif (( $attend==1 ))
+then
+	echo "Employee Par time"
+	salary=$(( $wagePrHr*$HalfDayHr ))
 else
-	echo "Employee Present"
+	echo "Employee Full time"
 	salary=$(( $wagePrHr*$FullDayHr ))
 fi
 echo $salary
